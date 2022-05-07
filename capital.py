6599dc82932efb4922ecf7c34f44870e1531d90a
Brian McRoberts <brimcrob@gmail.com>
2022-05-07 18:45:34 +0100

    2nd commit

---
+++ capital.py
@@ -0,0 +1,3 @@
+def capital_case(x):
+    return x.capitalize()
+
---
+++ test_First.py
@@ -0,0 +1,19 @@
+import pytest
+from capital import capital_case
+
+def test_capital_case():
+
+
+@pytest.mark.smoke
+def test_login():
+    print("Log in to the Application")
+
+
+@pytest.mark.regression
+def test_add_product():
+    print('add product')
+
+
+@pytest.mark.smoke
+def test_logout():
+    print('log out')
